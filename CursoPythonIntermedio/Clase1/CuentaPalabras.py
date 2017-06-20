92b9a057cd491d7e02638d78901b3e681040569b
Vanish03 <va_nia_@hotmail.com>
2017-06-19 20:28:35 -0500

    Anexo tarea 1 de Diccionario a traves de un archivo de python intermedio

--- CursoPythonIntermedio/Clase1/CuentaPalabras.py
+++ CursoPythonIntermedio/Clase1/CuentaPalabras.py
@@ -1,6 +1,6 @@
 #Cuenta Palabras
 
-import sys
+"""import sys
 
 nombreArchiv=sys.argv[1]
 
@@ -32,6 +32,38 @@ lista=cadenas.split()
 
 for palabra in lista:
 	totalS+=len(palabra)
+
+print("La cantidad de palabras es: ",len(lista))
+print("La cantidad de caracteres es: ",totalS)"""
+
+import sys
+
+nombreArch=sys.argv[1]
+
+f=open(nombreArch,"r")
+
+totalP=0
+totalC=0
+
+for linea in f.readlines():
+	totalP+=len(linea.split())
+	#totalP=totalP+len(linea.split())
+	for palabra in linea:
+		totalC+=len(palabra.strip())
+
+print("La cantidad de palabras es: ",totalP)	
+print("La cantidad de caracteres es: ",totalC)	
+
+
+f.seek(0)
+totalS=0
+
+cadenas=f.read()
+print(str(cadenas).split())
+lista=cadenas.split()
+
+for palabra in lista:
+	totalS += len(palabra)
 
 print("La cantidad de palabras es: ",len(lista))
 print("La cantidad de caracteres es: ",totalS)
